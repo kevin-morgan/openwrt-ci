105a34a16060fd470f038572d99926e691a3207b
Kevin MORGAN <kevin_morgan@outlook.com>
2020-02-28 20:16:01 +0800

    TEST Compile
    Signed-off-by: Kevin MORGAN <kevin_morgan@outlook.com>

--- custom.sh
+++ custom.sh
@@ -9,7 +9,7 @@
 # =================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
 
 
