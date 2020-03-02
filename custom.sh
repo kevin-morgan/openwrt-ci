8b3a316416d4615a29d467826804a4cc2d8ec812
Kevin MORGAN <kevin_morgan@outlook.com>
2020-03-02 16:49:46 +0800

    4th
    Signed-off-by: Kevin MORGAN <kevin_morgan@outlook.com>

--- custom.sh
+++ custom.sh
@@ -11,8 +11,13 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
+# Modify hostname
+sed -i 's/OpenWrt/MORGAN/g' package/base-files/files/bin/config_generate
 
+# Modify timezone
+sed -i 's/UTC/CST/g' package/base-files/files/bin/config_generate
 
-
+# Modify NTP Server
+sed -i 's/ntp1.aliyun.com/time.asia.apple.com/g' package/base-files/files/bin/config_generate
 
 
